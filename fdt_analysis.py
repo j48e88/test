a4ab43bea367195c348128f00edd3999529d75d0
j48e88 <121725251+j48e88@users.noreply.github.com>
2023-04-12 21:51:44 +0800

    Update fdt_analysis.py

--- fdt_analysis.py
+++ fdt_analysis.py
@@ -336,7 +336,7 @@ if uploaded_file is not None:
     # Update the content based on the checkbox value
     if show_content1:
         st.dataframe(pd.DataFrame(page_data), height=280)
-        show_content2 = st.checkbox("Show the statistic result on crew requirement")  
+        show_content2 = st.checkbox("Show the largest and smallest number of crew requirements in this month")  
         if show_content2:
             # print the crew information and the date and required crew number with the maximum crew number
             st.write(crew_table)
