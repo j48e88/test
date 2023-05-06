1fa100a82cedf6cb374d0b4e51e76393ec60e148
j48e88 <121725251+j48e88@users.noreply.github.com>
2023-05-06 13:55:47 +0800

    Update fdt_analysis.py

--- fdt_analysis.py
+++ fdt_analysis.py
@@ -91,11 +91,11 @@ if uploaded_file is not None:
     st.write("The reversed excel data is as follow:")
     st.write(df)
 
-    def calculate_num_layover(dep, arr, date, ac_type):
-        dep, arr = sorted([dep, arr])
+    def calculate_num_layover(count, dep, arr, date):
+        dep, arr, date = sorted([dep, arr, date])
+        length = dep, arr, date
         layover_count = 0
-        count = sorted([(dep, arr), (date, ac_type,)])
-        if len(count) % 2 == 1:
+        if len(length) % 2 == 1:
             layover_count += 1
         return layover_count
 
@@ -150,7 +150,7 @@ if uploaded_file is not None:
         dep = group[1][0]
         arr = group[1][1]
         ac_type = group[2]
-        num_layovers[(group[0], ac_type)] = calculate_num_layover(dep, arr, date, ac_type)
+        num_layovers[(group[0], ac_type)] = calculate_num_layover(count, dep, arr, date)
 
     # Calculate number of non-regular flights for each date
     num_nonregular = {}
